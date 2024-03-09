ac37aa9c9b0c0a6f2575826cc2c6f3340eb473a5
Ryan Hung <ryanhungryan@gmail.com>
2024-03-09 11:20:45 +0000

    comment on format on save

---
+++ lua/plugins/null-ls.lua
@@ -0,0 +1,31 @@
+return {
+  "nvimtools/none-ls.nvim",
+  dependencies = 'nvim-lua/plenary.nvim',
+  config = function()
+    local null_ls = require("null-ls")
+    local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
+    null_ls.setup({
+      -- format on save
+      -- you can reuse a shared lspconfig on_attach callback here
+    on_attach = function(client, bufnr)
+        if client.supports_method("textDocument/formatting") then
+            vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
+            vim.api.nvim_create_autocmd("BufWritePre", {
+                group = augroup,
+                buffer = bufnr,
+                callback = function()
+                    -- on 0.8, you should use vim.lsp.buf.format({ bufnr = bufnr }) instead
+                    -- on later neovim version, you should use vim.lsp.buf.format({ async = false }) instead
+                    vim.lsp.buf.formatting_sync()
+                end,
+            })
+        end
+    end,
+      sources = {
+        null_ls.builtins.formatting.black,
+        null_ls.builtins.formatting.djlint,
+        null_ls.builtins.formatting.prettier
+      }
+    })
+  end
+}
