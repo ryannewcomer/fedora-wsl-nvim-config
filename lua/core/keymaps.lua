ee8365dee7b7a7b42f76a2824f52af16eb8550ef
Ryan Hung <ryanhungryan@gmail.com>
2024-03-11 19:22:21 +0000

    add more telescope keymap

--- lua/core/keymaps.lua
+++ lua/core/keymaps.lua
@@ -1,5 +1,6 @@
 local keymap = vim.keymap
 local opts = { noremap = true, silent = true }
+local builtin = require("telescope.builtin")
 
 -- Increment/decrement
 keymap.set("n", "<C-+>", "<C-a>")
@@ -13,9 +14,13 @@ keymap.set("n", "te", ":tabedit")
 keymap.set("n", "<tab>", ":tabnext<Return>", opts)
 keymap.set("n", "<s-tab>", ":tabprev<Return>", opts)
 
-
 -- Diagnostics
 keymap.set("n", "<C-j>", function()
-	vim.diagnostic.goto_next()
+  vim.diagnostic.goto_next()
 end, opts)
 
+-- plugin
+
+-- Telescope
+keymap.set("n", "<leader>tf", builtin.find_files({}))
+keymap.set("n", "<leader>tl", builtin.live_grep({}))
